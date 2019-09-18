bb161d020c20fd179c68d67a79a465bfc810da75
Rick Leos <richardleos@outlook.com>
2019-09-17 17:58:18 -0700

    Add lesson 1 script file

---
+++ Lesson1.ps1
@@ -0,0 +1,114 @@
+#Comment starts with # before text
+
+<#
+If you want
+multiple line of
+comments start with <#
+then end with #>
+#>
+
+
+#Checck what account is used to run the powershell session
+whoami
+
+#varable
+$myvarable = 'Hello'
+$myvarable = 'annyawhnghasehyo'
+
+$myvarable2 = 'annyawhnghasehyo'
+
+#Built in varable Null
+$null
+
+#Using null varable to null a varable
+$myvarable = $null
+
+#Enviorment varable, gets computer name
+$env:COMPUTERNAME 
+$env:USERNAME
+
+$computername = $env:COMPUTERNAME
+$username = $env:USERNAME
+
+
+#Array a collection of varables or objects
+$myarry = 'Hello', 'annyawhnghasehyo'
+
+#View first item in as array
+$myarry[0]
+#View second item in an array
+$myarry[1]
+
+#Create an empaty Arryay
+$myarry2 = @()
+#View array
+$myarry2
+
+#Add variables to array
+$myarry2 += $myvarable
+$myarry2 += $myvarable2
+
+
+#Object is a collection of known attributes/properties to consume
+Get-WmiObject Win32_OperatingSystem
+
+#Add object to Array
+$os = Get-WmiObject Win32_OperatingSystem
+#view Array conatining object
+$os
+
+#View a property of an object
+$os.BuildNumber
+$os.Version
+#View all properties of an object
+$os.Properties
+#View all properties of an object by Name
+$os.Properties.Name
+
+#View Install Date hidden property
+$os.InstallDate  
+
+#Create an object
+$myobject = New-Object -TypeName PSObject
+#Add object data to your object
+Add-Member -InputObject $myobject -MemberType NoteProperty -Name MYPCOperatingSystem -Value $os.Name
+Add-Member -InputObject $myobject -MemberType NoteProperty -Name MyPCVersion -Value $os.Version
+Add-Member -InputObject $myobject -MemberType NoteProperty -Name MYPCInstallDate -Value $os.InstallDate
+Add-Member -InputObject $myobject -MemberType NoteProperty -Name MYPCName -Value $os.CSName
+Add-Member -InputObject $myobject -MemberType NoteProperty -Name MYPCUser -Value $env:USERNAME
+
+
+
+#View created object
+$myobject
+#View variables in object
+$myobject.MYPCOperatingSystem
+$myobject.MyPCVersion
+$myobject.MYPCInstallDate
+$myobject.MYPCName
+$myobject.MYPCUser
+
+
+#Modules are scripts that contain functions to get data from specific resrouces
+#Import Active Directory module
+Import-Module ActiveDirectory
+
+#Lookup computer in AD 
+get-adcomputer -Identity putil4
+#Lookup computer in AD using Enviorment varable
+Get-ADComputer -Identity $env:COMPUTERNAME 
+#Lookup computer in AD using property in custom object
+Get-ADComputer -Identity $myobject.MYPCName
+
+#Troubleshooting powershell errors
+#Lookup computer in AD will bad object
+Get-ADComputer -Identity $myobject.MYPChostname
+
+#Lookup computer in AD fails, not found in domain
+Get-ADComputer -Identity DCCS-RDB
+
+Get-ADComputer -Identity DCCS-RDB -Server
+
+
+
+
