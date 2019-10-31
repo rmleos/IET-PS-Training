d54ea4cd75cc6b9883ad835778aa660ba41b15a5
Rick Leos <rmleos@udavis.edu>
2019-10-31 12:05:10 -0700

    Losson5 complete

--- Lesson5.ps1
+++ Lesson5.ps1
@@ -2,12 +2,10 @@
 $list = import-csv -Path C:\Users\rick-srv\Documents\serverlist.csv
 #removes existing pssessions
 Get-PSSession | Remove-PSSession
-
 #Create Variable for each server in a list and PSSession to it. Then save the PSSession  to the Variable
 foreach ($system in $list)
 {
     $session = New-PSSession -ComputerName $system.ServerName -Name $system.ServerName
-    $server = $system.ServerName
     $localLapsfile = @()
     $localLapsDLL = @()
     #Invoke command get info about LAPS install and MSI file
@@ -26,7 +24,7 @@ foreach ($system in $list)
     }
     $localLapsDLL = invoke-Command -Session $session -ScriptBlock {
         #Check in the LAPS client is insall to default path
-        if ((Get-ChildItem -Path C:\Program Files\LAPS\CSE -ErrorAction SilentlyContinue).Name -contains 'AdmPwd.dll')
+        if ((Get-ChildItem -Path 'C:\Program Files\LAPS\CSE' -ErrorAction SilentlyContinue).Name -contains 'AdmPwd.dll')
         {
             #Write-Host "LAPS DLL is present on $server." -ForegroundColor Cyan
              $true
@@ -43,41 +41,73 @@ foreach ($system in $list)
     #If LAPS is not installed and LAPS msi is not in c:\Deploy, it will copy file and installing LAPS
     if ($localLapsDLL -eq $false -and $localLapsfile -eq $false) {
         Copy-Item "C:\Deploy\LAPS.x64.msi" -ToSession $session -Destination "C:\Deploy\"
-        invoke-Command -Session $session -ScriptBlock {
+        $localLapsfile = invoke-Command -Session $session -ScriptBlock {
+            #Checks for the LAPS install file
             if ((Get-ChildItem -Path C:\Deploy -ErrorAction SilentlyContinue).Name -contains 'LAPS.x64.msi')
             {
-                Write-Host "LAPS msi file is present on $server starting install." -ForegroundColor Cyan
-                #Local LAPS agent
-                $localfilesource = 'C:\Deploy\LAPS.x64.msi'
-                #Arguments for installer
-                $ArgumentList = '/quiet'
-                #command to run proccess for MSI and Arguments for installer "msiexec /i <file location>\LAPS.x64.msi /quiet"
-                Start-Process $localfilesource -ArgumentList $ArgumentList
+                #Write-Host "LAPS msi is present on $server." -ForegroundColor Cyan
+                $true
             }
-            #Provides output if copy if not work
-        }    
+            else
+            {
+                #Write-Host "LAPS msi is not present on $server." -ForegroundColor Yellow
+             $false
+            }
+        }
+        if ($localLapsDLL -eq $false -and $localLapsfile -eq $true) {
+            
+            invoke-Command -Session $session -ScriptBlock {
+                if ((Get-ChildItem -Path C:\Deploy -ErrorAction SilentlyContinue).Name -contains 'LAPS.x64.msi')
+                {
+                    Write-Host "LAPS msi file is present on $env:COMPUTERNAME starting install." -ForegroundColor Cyan
+                    #Local LAPS agent
+                    $localfilesource = 'C:\Deploy\LAPS.x64.msi'
+                    #Arguments for installer
+                    $ArgumentList = '/quiet'
+                    #command to run proccess for MSI and Arguments for installer "msiexec /i <file location>\LAPS.x64.msi /quiet"
+                    Start-Process $localfilesource -ArgumentList $ArgumentList
+                }
+            }
+            Start-Sleep -Seconds 10
+            #Recheck of LAPS DLL
+            invoke-Command -Session $session -ScriptBlock {
+                #Check in the LAPS client is insall to default path
+                if ((Get-ChildItem -Path 'C:\Program Files\LAPS\CSE' -ErrorAction SilentlyContinue).Name -contains 'AdmPwd.dll')
+                {
+                    Write-Host "LAPS DLL is present on $env:COMPUTERNAME ." -ForegroundColor Cyan
+                }
+                else {
+                    Write-Host "LAPS DLL is non present on $env:COMPUTERNAME ." -ForegroundColor Yellow             
+                }
+            }
+        }
     }
-    else {
-        Write-Host "LAPS msi did not copy to $server." -ForegroundColor Yellow 
-    }
-
 
 
     #If LAPS is not installed and LAPS msi is in c:\Deploy, it will start installing LAPS
     else {
         if ($localLapsDLL -eq $false -and $localLapsfile -eq $true ) {
-            Write-Host "LAPS msi file is present on $system starting install." -ForegroundColor Cyan
+        invoke-Command -Session $session -ScriptBlock {
+            Write-Host "LAPS msi file is present on $env:COMPUTERNAME starting install." -ForegroundColor Cyan
             #Local LAPS agent
             $localfilesource = 'C:\Deploy\LAPS.x64.msi'
             #Arguments for installer
             $ArgumentList = '/quiet'
             #command to run proccess for MSI and Arguments for installer "msiexec /i <file location>\LAPS.x64.msi /quiet"
             Start-Process $localfilesource -ArgumentList $ArgumentList
+            }
         }
-        else {
-            Write-Host "LAPS is already install on $system." -ForegroundColor Cyan
+        Start-Sleep -Seconds 10
+        #Recheck for LAPS DLL
+        invoke-Command -Session $session -ScriptBlock {
+            #Check in the LAPS client is insall to default path
+            if ((Get-ChildItem -Path 'C:\Program Files\LAPS\CSE' -ErrorAction SilentlyContinue).Name -contains 'AdmPwd.dll')
+            {
+                Write-Host "LAPS DLL is present on $env:COMPUTERNAME ." -ForegroundColor Cyan
+            }
+            else {
+                Write-Host "LAPS DLL is non present on $env:COMPUTERNAME ." -ForegroundColor Yellow             
+            } 
         }
-     
-        
     }
 }
