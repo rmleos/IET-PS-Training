1350b17cb228843d346211b4939e5fce3bb1247e
Rick Leos <rmleos@udavis.edu>
2019-12-12 09:52:47 -0800

    Add lesson 9

---
+++ Lesson9.ps1
@@ -0,0 +1,6 @@
+#Working with Certificates in PowerShell
+Start-Process iexplore.exe 'https://docs.microsoft.com/en-us/powershell/scripting/dsc/overview/overview?view=powershell-5.1'
+Start-Process chrome.exe 'https://docs.microsoft.com/en-us/powershell/scripting/dsc/getting-started/wingettingstarted?view=powershell-5.1'
+Start-Process chrome.exe 'https://docs.microsoft.com/en-us/powershell/scripting/dsc/quickstarts/website-quickstart?view=powershell-5.1'
+Start-Process chrome.exe 'https://blogs.technet.microsoft.com/privatecloud/2013/08/30/introducing-powershell-desired-state-configuration-dsc/'
+Start-Process chrome.exe 'https://docs.ansible.com/ansible/latest/user_guide/windows_dsc.html'
