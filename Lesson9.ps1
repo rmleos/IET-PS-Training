71ec26c792907a62fd49ae03171b0755150070b0
Rick Leos <rmleos@udavis.edu>
2019-12-12 09:56:50 -0800

    Edit Lesson 9

--- Lesson9.ps1
+++ Lesson9.ps1
@@ -1,6 +1,12 @@
-#Working with Certificates in PowerShell
+#Windows PowerShell Desired State Configuration Overview
 Start-Process iexplore.exe 'https://docs.microsoft.com/en-us/powershell/scripting/dsc/overview/overview?view=powershell-5.1'
+#Get started with Desired State Configuration (DSC) for Windows
 Start-Process chrome.exe 'https://docs.microsoft.com/en-us/powershell/scripting/dsc/getting-started/wingettingstarted?view=powershell-5.1'
+#DSC Resources
+Start-Process chrome.exe 'https://docs.microsoft.com/en-us/powershell/scripting/dsc/resources/resources?view=powershell-5.1'
+#Quickstart - Create a website with DSC
 Start-Process chrome.exe 'https://docs.microsoft.com/en-us/powershell/scripting/dsc/quickstarts/website-quickstart?view=powershell-5.1'
+#Introducing PowerShell Desired State Configuration (DSC)
 Start-Process chrome.exe 'https://blogs.technet.microsoft.com/privatecloud/2013/08/30/introducing-powershell-desired-state-configuration-dsc/'
+#Ansible DSC
 Start-Process chrome.exe 'https://docs.ansible.com/ansible/latest/user_guide/windows_dsc.html'
